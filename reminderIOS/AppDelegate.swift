9ecbdf704e25307bd1b81686a06811757ec7c1c6
mertkanak <mertkanak@anadoluhayat.com.tr>
2024-01-22 00:53:56 +0300

    initial commit

--- reminderIOS/AppDelegate.swift
+++ reminderIOS/AppDelegate.swift
@@ -11,6 +11,8 @@ import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    
+    //bapsss
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
